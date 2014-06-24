a08aa1295eef6a8671c4761af9e2efa3662c70b9
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2014-06-24 10:47:30 +0200

    Cleaned up 4.8 release and ignore all temp files

--- ADEPO-build-desktop-Qt_4_8_1_dans_le_PATH__Syst_me__Release/bash_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-cp scripts_lwdaq/Acquisifier_params.tcl /home/cern-mehdi/ADEPO/input_folder/LWDAQ/LWDAQ.app/Contents/LWDAQ/Startup/
-cp scripts_lwdaq/Acquisifier_Settings.tcl /home/cern-mehdi/ADEPO/input_folder/LWDAQ/Tools/Data/
-cp scripts_lwdaq/Acquisifier_script_file.tcl /home/cern-mehdi/ADEPO/input_folder/LWDAQ/Tools/Data/
-/home/cern-mehdi/ADEPO/input_folder/LWDAQ/lwdaq --no-console 
-ps -eH | grep tclsh8.5 > PID.txt 
-read pid reste < PID.txt 
-sleep 30s
-kill "$pid" 
