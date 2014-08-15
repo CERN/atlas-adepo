ce86725fb4ec8e47cab9f2bcdadc0011de750a9f
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2014-08-15 15:27:24 +0200

    Debugging 3 prism output

--- ADEPO/src/img_coord_to_bcam_coord.cpp
+++ ADEPO/src/img_coord_to_bcam_coord.cpp
@@ -22,7 +22,7 @@ void img_coord_to_bcam_coord(bdd & base_donnees)
             // NumChip == 2 is Z+ direction (check?)
             int num_chip = base_donnees.getBCAM(spot.getBCAM())->getNumChip();
             bool directionOk = ((num_chip == 2) && (calib1.getCoordAxis().z() > 0)) || ((num_chip == 1) && (calib1.getCoordAxis().z() < 0));
-            std::cout << spot.getBCAM() << " " << num_chip << " " <<  directionOk << " " << calib1.getCoordAxis().z() << std::endl;
+            std::cout << spot.getBCAM() << " " calib1.getBCAM() << directionOk << " " <<  num_chip << " " << calib1.getCoordAxis().z() << std::endl;
 
             // check for name and direction.
             if (spot.getBCAM() == calib1.getBCAM() && directionOk)
