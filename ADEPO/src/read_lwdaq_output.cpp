3046e5f0770ef5e22c122a4cf451092c0fdce469
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2014-08-15 10:57:33 +0200

    Debugging 3 prism output

--- ADEPO/src/read_lwdaq_output.cpp
+++ ADEPO/src/read_lwdaq_output.cpp
@@ -17,6 +17,7 @@ int read_lwdaq_output(QFile &file, bdd & base_donnees)
             if(!ligne.empty())
             {
                 int nb_colonnes = std::count(ligne.begin(), ligne.end(), ' '); //je compte le nombre de colonnes dans le fichier
+                std::cout << "nb " << nb_colonnes << std::endl;
 
                 //2 spots
                 if(nb_colonnes == 12)
