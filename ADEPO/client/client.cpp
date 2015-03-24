b462b26a6b79fa0b0d19c8a1c768f50ec4d5cc2d
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2015-03-24 10:37:53 +0100

    Disabled full precision while calc

--- ADEPO/client/client.cpp
+++ ADEPO/client/client.cpp
@@ -90,8 +90,8 @@ void Client::setEnabled() {
     ui->stop->setEnabled(canStop && run.getMode() == MODE_CLOSURE);
     ui->monitoringStop->setEnabled(canStop && run.getMode() == MODE_MONITORING);
 
-    ui->reset->setEnabled(canStart);
-
+    ui->reset->setEnabled(enabled);
+    ui->fullPrecision->setEnabled(enabled);
     ui->tableWidget_liste_detectors->setEnabled(enabled);
     ui->mode->setEnabled(enabled);
     ui->airpad->setEnabled(enabled);
